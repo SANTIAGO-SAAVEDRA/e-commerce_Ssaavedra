5d4c6e76b47b91b1c720d72285d13897d859c2c0
Santiago Saavedra <santiagogsaavedra019@gmail.com>
2021-07-26 22:41:51 -0300

    jsx aplicado

---
+++ src/NavBar.js
@@ -0,0 +1,33 @@
+const NavBar = () => 
+<>
+    <h1>LOGO</h1>
+        <nav id="barraNav">
+            <ul>
+                <li>
+                    <a href="#">INICIO</a>
+                </li>
+                <li>
+                    <a href="#">NOSOTROS</a>
+                </li>
+                <li>
+                    <a href="#">INFO</a>
+                </li>
+                <li>
+                    <a href="#">CONTACTO</a>
+                </li>
+            </ul>
+        </nav>
+
+    {/* <style>
+        
+    #barraNav ul li{
+    list-style: none;
+    margin-left: 75px;
+    padding: 25px 0;
+    float: left;
+}
+
+    </style> */}
+</>
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//COMMON JS
+//const React = require('react';)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//ES6
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1) TENER REACT EN SCOPE
+import React from "react"
+
+// 2) Tener REACT_DOM en scope
+import ReactDOM from "react-dom"
+
+
+import NavBar from "./NavBar.js" 
+// 3) Crear un componente
+// const App = () => "Hola Mundo"
+
+// 4) Renderizar la aplicación UNA ÚNICA VEZ
+ReactDOM.render(<NavBar/>, document.getElementById('root'))
